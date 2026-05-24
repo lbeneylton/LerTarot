54a7a3003c9d0e45bbcebeebdd399265c87a1a05
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-05-23 23:30:24 -0300

    dev: alembic criado e configurado

---
+++ alembic/env.py
@@ -0,0 +1,76 @@
+from ..src.lertarot.core import Base  # Importação da classe Base de metadados
+
+# garante carregamento de todos os models
+
+# Importação das configurações do banco
+from ..src.lertarot.core import database_settings
+
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline() -> None:
+    """Modo offline (gera SQL sem conectar no banco)."""
+    url = database_settings.url_database
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Modo online (executa no banco real)."""
+    connectable = engine_from_config(
+        {
+            "sqlalchemy.url": database_settings.url_database  # 👈 usa settings aqui
+        },
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,  # recomendado
+            compare_server_default=True  # recomendado
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
