81a7cef40da7848fedcd3ce858fe4b254758b66f
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-05-23 23:30:46 -0300

    dev: init do core ajustado

--- src/lertarot/core/__init__.py
+++ src/lertarot/core/__init__.py
@@ -1,5 +1,20 @@
-from .settings.database import database_settings
+"""Transforma a pasta core em um módulo
+e facilita a importação dessas classes, metodos e instancias:"""
+
+# Instância do objeto de configuração do JWT
 from .settings.security import security_settings
+
+# Instância do objeto de configuração do banco
+from .settings.database import database_settings
+
+# Instância
 from .logger.logger import logger as lg
+
+# Função geradora de sessões com gerenciador de contexto
 from .database.session import get_session
+
+# Classe de error de domain basico
 from .exception import DomainError
+
+# Classe para metadados dos models
+from .database.base import Base
--- src/lertarot/core/exception.py
+++ src/lertarot/core/exception.py
@@ -1,9 +1,9 @@
 """Classe Base dos erros de dominio
 Por padrão o status_code será 400
 e code DOMAIN_ERROR, 
-que pode ser alterado a partir das heranças de cada dominio
+que pode ser alterado a partir das heranças de cada dominio"""
 
-"""
+
 class DomainError(Exception):
 
     code = "DOMAIN_ERROR"
