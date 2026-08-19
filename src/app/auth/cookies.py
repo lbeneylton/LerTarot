01f83c2eef61a8d9c37e3c612b3f32623597b3d9
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-08-18 21:29:02 -0300

    cookie manager

--- src/app/auth/cookies.py
+++ src/app/auth/cookies.py
@@ -1,6 +1,9 @@
 # app/auth/cookies.py
 
+from datetime import timedelta
 from fastapi import Response, Cookie
+
+from app.core.config import settings
 
 from app.core.exceptions import UnauthorizedError
 
@@ -8,68 +11,82 @@ from app.security.jwt_provider import JwtTokenService
 from app.users.services import UserService
 
 
-ACCESS_MAX_AGE = 15 * 60
-REFRESH_MAX_AGE = 7 * 24 * 60 * 60
-
+# PATHS ROOTS 
 ACCESS_PATH = "/"
 REFRESH_PATH = "/auth/refresh"
 
-# HTTPS
-SECURE = False
+class CookieManager:
+    def __init__(self, access_age:int, refresh_age:int, secure:bool = True) -> None:
+        self.access_age = access_age
+        self.refresh_age = refresh_age
+        self.secure = secure # HTTPS
+    
+    def set_auth_cookies(
+        self,
+        response: Response,
+        access_token: str,
+        refresh_token: str,
+    ) -> None:
+        response.set_cookie(
+            key="access_token",
+            value=access_token,
+            httponly=True,
+            secure=self.secure,
+            samesite="lax",
+            max_age=self.access_age,
+            path=ACCESS_PATH,
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=self.secure,
+            samesite="lax",
+            max_age=self.refresh_age,
+            path=REFRESH_PATH,
+        )
 
 
-def set_auth_cookies(
-    response: Response,
-    access_token: str,
-    refresh_token: str,
-) -> None:
-    response.set_cookie(
-        key="access_token",
-        value=access_token,
-        httponly=True,
-        secure=SECURE,
-        samesite="lax",
-        max_age=ACCESS_MAX_AGE,
-        path=ACCESS_PATH,
-    )
+    def clear_auth_cookies(self, response: Response) -> None:
+        response.delete_cookie(
+            key="access_token",
+            path=ACCESS_PATH,
+        )
 
-    response.set_cookie(
-        key="refresh_token",
-        value=refresh_token,
-        httponly=True,
-        secure=SECURE,
-        samesite="lax",
-        max_age=REFRESH_MAX_AGE,
-        path=REFRESH_PATH,
-    )
+        response.delete_cookie(
+            key="refresh_token",
+            path=REFRESH_PATH,
+        )
 
 
-def clear_auth_cookies(response: Response) -> None:
-    response.delete_cookie(
-        key="access_token",
-        path=ACCESS_PATH,
-    )
+# def get_current_user(
+#         token_provider: JwtTokenService,
+#         user_service: UserService,
+#         access_token: str | None = Cookie(default=None)
+# ):
 
-    response.delete_cookie(
-        key="refresh_token",
-        path=REFRESH_PATH,
-    )
+#     if not access_token:
+#         raise UnauthorizedError("Não autenticado")
+
+#     payload = token_provider.decode_access_token(access_token)
+
+#     if not payload:
+#         raise UnauthorizedError("Token inválido")
+
+#     user_id = int(payload["sub"])
+
+#     return user_service.current_user(user_id)
 
 
-def get_current_user(
-        token_provider: JwtTokenService,
-        user_service: UserService,
-        access_token: str | None = Cookie(default=None)
-):
 
-    if not access_token:
-        raise UnauthorizedError("Não autenticado")
+# MAX AGES
+ACCESS_MAX_AGE = int(timedelta(minutes=settings.access_expire_minutes).total_seconds())
+REFRESH_MAX_AGE = int(timedelta(days=settings.refresh_expire_days).total_seconds())
 
-    payload = token_provider.decode_access_token(access_token)
 
-    if not payload:
-        raise UnauthorizedError("Token inválido")
-
-    user_id = int(payload["sub"])
-
-    return user_service.current_user(user_id)
+cookie_manager = CookieManager(
+    ACCESS_MAX_AGE,
+    REFRESH_MAX_AGE,
+    False
+)
