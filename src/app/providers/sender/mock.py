b79f26b4c1530542618c70509079561c50f6d25e
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-08-22 19:39:56 -0300

    mock sender emial ok

---
+++ src/app/providers/sender/mock.py
@@ -0,0 +1,14 @@
+import logging
+from app.providers.sender.interface import EmailSender
+
+logger = logging.getLogger("app.email_mock")
+
+class MockEmailSender(EmailSender):
+    def send(self, to: str, subject: str, body: str) -> None:
+        logger.info(
+            f"=== [MOCK EMAIL SENDER] ===\n"
+            f"Para: {to}\n"
+            f"Assunto: {subject}\n"
+            f"Corpo:\n{body}\n"
+            f"==========================="
+        )
