30985c1d34a4e8f7e6228f7a68fe24ed507336f8
Lucas Beneylton <47756391+lbeneylton@users.noreply.github.com>
2026-08-23 17:59:18 -0300

    updates

--- src/providers/email_sender/mock.py
+++ src/providers/email_sender/mock.py
@@ -4,7 +4,24 @@ from providers.email_sender.get_sender import EmailSender
 logger = logging.getLogger("app.email_mock")
 
 class MockEmailSender(EmailSender):
-    def send(
+    def send_text(
+        self, to: 
+        str, subject: str | None, 
+        body: str | None,
+        template: str | None,
+        variables: str | None
+        
+        ) -> None:
+        logger.info(
+            f"=== [MOCK EMAIL SENDER] ===\n"
+            f"Para: {to}\n"
+            f"Assunto: {subject}\n"
+            f"Corpo:\n{body}\n"
+            f"Template:\n{template}\n"
+            f"variables:\n{variables}\n"
+            f"==========================="
+        )
+    def send_template(
         self, to: 
         str, subject: str | None, 
         body: str | None,
